163afd5b6232abd0ad91cc5e6f461cfa2f5fcaef
fadianjrou <fadianjrou@gmail.com>
2022-10-29 13:47:54 +0300

    Model 2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -6,7 +6,7 @@ def model1():
     print("adding code for ML model 1")
 
 def model2():
-    print("adding code for ML model 1")
+    print("adding code for ML model 2")
 
 def model3():
-    print("adding code for ML model 1")
+    print("adding code for ML model 3")
