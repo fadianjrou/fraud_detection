e500778dff88fcf76ec00bbf869b5917bfd3c7c2
fadianjrou <fadianjrou@gmail.com>
2022-10-29 13:19:41 +0300

    added 3 new functions for ML

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("adding all ml releated code here")
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 1")
+
+def model3():
+    print("adding code for ML model 1")
